1364e9989ecc2e56243dc79ccd224b4a288457e3
Zavion Sherman <zsherman2510@hotmail.com>
2023-05-14 12:36:28 -0700

    payments page complete

--- src/components/Pages/Payments.jsx
+++ src/components/Pages/Payments.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import TableComponent from '../UIComponents/TableComponent'
 
 const Payments = () => {
+
+  const [ payments, setPayments] = useState([])
+  const [ error, setError ] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      try {
+        const response  = await fetch('data.json');
+        const { payments } = await response.json();
+        setPayments(payments);
+      } catch(error) {
+        setError(error);
+      }
+      
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Payments</div>
+    <div className="table-widget">
+      <h2>Payments</h2>
+      {payments.length === 0 ? (
+        <p>No Payments</p>
+      ) : (
+        <TableComponent data={payments} />
+      )}
+    </div>
   )
 }
 
